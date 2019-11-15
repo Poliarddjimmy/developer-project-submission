957bfcd3e0732aab4b3eaf51fdb27dcb932eaba3
POLIARD DJimmy <poliarddjimmy@gmail.com>
2019-11-15 12:16:29 -0500

    Update view files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :videos, class_name: "Video", foreign_key: "user_id"
+
+    has_secure_password
+    validates :email , presence: true, uniqueness: true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,6 @@
 class Video < ApplicationRecord
+    belongs_to :user, class_name: "User", foreign_key: "user_id"
+    YT_LINK_FORMAT = /\A.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/i
+
+    validates :link, presence: true, format: YT_LINK_FORMAT, uniqueness: true
 end
